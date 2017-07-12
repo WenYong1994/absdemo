f413fea65c44957dd7e1cd64b8fbffa49b8bea77
wenyong1994 <779889925@qq.com>
2017-07-12 16:08:36 +0800

    修改为绑定布局文件的问题

--- src/main/java/com/example/warehouse/abswymode/activity/BackActivity.java
+++ src/main/java/com/example/warehouse/abswymode/activity/BackActivity.java
@@ -1,6 +1,14 @@
 package com.example.warehouse.abswymode.activity;
 
+import android.os.Bundle;
+
 public abstract class BackActivity extends BaseBackActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(initView());
+    }
+
     protected abstract int initView();
 
     protected abstract void initData();
