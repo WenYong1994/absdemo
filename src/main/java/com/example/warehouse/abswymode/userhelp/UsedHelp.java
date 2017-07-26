ff3d7f82a61fce9fc4bbd2cdd18097934c79c0a9
wenyong1994 <779889925@qq.com>
2017-07-26 16:41:02 +0800

    提交模仿苹果的3d滑动效果选择器

---
+++ src/main/java/com/example/warehouse/abswymode/userhelp/UsedHelp.java
@@ -0,0 +1,25 @@
+package com.example.warehouse.abswymode.userhelp;
+
+/**
+ * Created by Administrator on 2017/7/26.
+ */
+
+public class UsedHelp {
+
+    //使用模仿苹果的3d滑动图
+
+    /*final ArrayList<String> arrayList = new ArrayList();
+        for(int i =0;i<5;i++){
+        arrayList.add("item"+i);
+    }
+        loopView.setItems(arrayList);
+        loopView.setListener(new OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(int index) {
+            Toast.makeText(BindCarNumberActivity.this, arrayList.get(index), Toast.LENGTH_SHORT).show();
+        }
+    });
+    //设置初始位置
+        loopView.setInitPosition(0);*/
+
+}
